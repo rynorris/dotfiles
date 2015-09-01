3cfa5b694c40e579f2a7a2e3ba2c9d00e43b9817
Ryan Norris <rynorris@gmail.com>
2015-09-01 16:07:50 +0100

    Add template vimrc_local and capability to install it if necessary.

--- install.sh
+++ install.sh
@@ -9,10 +9,19 @@ DOTFILE_DIR=$(dirname $(readlink -f ${BASH_SOURCE}))
 # If <name>_local already exists, bails out.
 #   $1 = dotfile
 #   $2 = target location
+#   $3 = if "noclobber", do nothing if target already exists.
 ###############################################################################
 install() {
   local dotfile="${DOTFILE_DIR}/$1"
   local target="$2"
+  local noclobber="$3"
+
+  # If noclobber is set, bail if target exists in any form.
+  if [[ -e $target && -n $noclobber ]]
+  then
+    echo "$target exists.  Leaving alone."
+    return 0
+  fi
 
   # If target is a symbolic link, just remove it.  Notify.
   if [[ -L $target ]]
@@ -21,17 +30,17 @@ install() {
     rm "$target"
   fi
 
-  # If target is a file, move it to ${target}_local unless that exists.
+  # If target is a file, move it to ${target}_backup unless that exists.
   if [[ -f $target ]]
   then
-    echo "Moving $target to ${target}_local"
-    if [[ -e ${target}_local ]]
+    echo "Moving $target to ${target}_backup"
+    if [[ -e ${target}_backup ]]
     then
-      echo "${target}_local exists.  Bailing."
+      echo "${target}_backup exists.  Bailing."
       return 1
     fi
 
-    mv "$target" "${target}_local"
+    mv "$target" "${target}_backup"
   fi
 
   # Now we're good to create the symbolic link to the dotfile here.
@@ -41,5 +50,6 @@ install() {
 
 # Actually install the dotfiles.
 install vimrc ~/.vimrc
+install vimrc_local ~/.vimrc_local noclobber
 install bashrc ~/.bashrc
 install gitconfig ~/.gitconfig
